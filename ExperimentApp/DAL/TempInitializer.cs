f5613b09d75e2c2a27717adedc726a061c333516
aviad <chaviva.moshavi@gmail.com>
2019-04-04 13:16:46 +0300

    final version

--- ExperimentApp/DAL/TempInitializer.cs
+++ ExperimentApp/DAL/TempInitializer.cs
@@ -8,7 +8,7 @@ using ExperimentApp.Infrastructure;
 
 namespace ExperimentApp.DAL
 {
-    public class TempInitializer : System.Data.Entity.DropCreateDatabaseAlways<ExperimentContext>
+    public class TempInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ExperimentContext>
     {
         protected override void Seed(ExperimentContext context)
         {}
